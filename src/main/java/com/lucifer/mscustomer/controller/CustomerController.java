2d38f85d1a7c11df59804e3166bd94fcab03c0a2
StarLord🧑‍🎤 <star@lord.es>
2024-10-13 23:35:39 -0500

    Inicio del Microservicio de customer

---
+++ src/main/java/com/lucifer/mscustomer/controller/CustomerController.java
@@ -0,0 +1,5 @@
+package com.lucifer.mscustomer.controller;
+
+public class CustomerController {
+
+}
---
+++ src/main/java/com/lucifer/mscustomer/entities/Customer.java
@@ -0,0 +1,10 @@
+package com.lucifer.mscustomer.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class Customer {
+
+}
